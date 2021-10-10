2112cfefa0b6775047c8c0efece6c87d5a16f3d5
Daniel Slattery <daniel.slattery1@gmail.com>
2021-10-10 17:59:07 +0100

    added git ignore to server

--- client/src/components/InputForm/InputForm.jsx
+++ client/src/components/InputForm/InputForm.jsx
@@ -1,37 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const InputForm = ({ createTopic }) => {
-
-  const [ title, setTitle ] = useState('');
-  const [ error, setError ] =useState(false);
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    if (title !== '') {
-      await createTopic({title})
-      setTitle('');
+    if (title !== "") {
+      await createTopic({ title });
+      setTitle("");
     } else {
       setError(true);
     }
-  }
+  };
 
   const onChangehandler = (e) => {
     setTitle(e.target.value);
     error && setError(false);
-  }
+  };
 
   return (
-      <form className='InputForm' onSubmit={submitHandler}>
-        <input
-        className={error ? 'error' : ''}
+    <form className="InputForm" onSubmit={submitHandler}>
+      <input
+        className={error ? "error" : ""}
         type="text"
         placeholder="Title"
         value={title}
         onChange={onChangehandler}
-        />
-        <button type='submit' className='plus-symbol'>➕</button>
-      </form>
-  )
-}
+      />
+      <button type="submit" className="plus-symbol">
+        ➕
+      </button>
+    </form>
+  );
+};
 
-export default InputForm
+export default InputForm;
